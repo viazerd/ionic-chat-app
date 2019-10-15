089c7d97f20b624cfca0bb8dec5438d60945dc6b
sandeep-experdel <sandeepvyas.34@gmail.com>
2019-10-15 18:50:54 +0530

    Finished logout

--- SocketServer/app/api/controllers/users.js
+++ SocketServer/app/api/controllers/users.js
@@ -3,12 +3,13 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 module.exports = {
+    //Register User
     create: (req,res,next)=> {
         userModel.find({email:req.body.email},function(err,data){
             if(data.length){
                 res.json({status:'error',message:'user already regsitered'})
             }else{
-                userModel.create({name:req.body.username,email:req.body.email,password:req.body.password,status:true},(err,result)=>{
+                userModel.create({name:req.body.username,email:req.body.email,password:req.body.password,status:true,token:null},(err,result)=>{
                     if(err)
                         {
                             next(err);
@@ -24,6 +25,7 @@ module.exports = {
         })
     },
 
+    //Authenticate and login user
     authenticate: (req,res,next) =>{
         userModel.findOne({email:req.body.email},(err,userInfo)=>{
             if(err)
@@ -31,12 +33,27 @@ module.exports = {
             else{
                 if(bcrypt.compareSync(req.body.password,userInfo.password)){
                     const token = jwt.sign({id:userInfo._id},req.app.get('secretKey'),{expiresIn:'1h'});
+                    userModel.findOneAndUpdate(req.body.email,{token:token},function(err,userInfo){
                     res.json({status:"success",message:"User Found!!",data:{user:userInfo, token:token}});
+                    })
                 } else{
                     res.json({status:"error",message:'Invalid Username/Password',data:null});
                 }
             }
         })
+    },
+
+    //Logout 
+    logout:(req,res,next) =>{
+        userModel.findByIdAndUpdate(req.params.userId,{status:false,token:null},function(err,userInfo){
+            if(err){
+                console.log(req.params.userId);
+                next(err)
+            }else{
+                res.json({status:"success",message:"Successfully Logged out",data:userInfo})
+            }
+        })
+        console.log(req.params.userId);
     },
 
     getAll:(req,res,next) =>{
@@ -47,7 +64,7 @@ module.exports = {
             }
             else{
                 for (let user of users){
-                    userList.push({id: user._id,name:user.name,password:user.password,status:user.status})
+                    userList.push({id: user._id,name:user.name,password:user.password,status:user.status,token:user.token})
                 }
                 res.json({status:'success',message:'users found',data:{users:userList}});
             }
--- SocketServer/routes/users.js
+++ SocketServer/routes/users.js
@@ -5,5 +5,6 @@ const userController = require('../app/api/controllers/users');
 router.post('/register',userController.create);
 router.post('/authenticate',userController.authenticate);
 router.get('/users',userController.getAll);
+router.put('/logout/:userId',userController.logout);
 
 module.exports = router;

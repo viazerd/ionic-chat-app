f7ac5fe3de5d8b1729c31d417bcc368112d0bc4c
sandeep-experdel <sandeepvyas.34@gmail.com>
2019-10-17 17:39:57 +0530

    Restarting with server from scratch

--- SocketServer/config/database.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost/node_rest_api';
-mongoose.connect(mongoDB,{useNewUrlParser:true});
-mongoose.Promise = global.Promise;
-module.exports = mongoose; 
